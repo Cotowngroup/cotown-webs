90ff4630617917aa50432f748bf355bd0ea09411
alejandro-andre <alejandroandref@gmail.com>
2025-10-06 12:52:23 +0200

    Promos v0.1

--- code/src/data/promos.js
+++ code/src/data/promos.js
@@ -1,5 +1,6 @@
 module.exports = async (config) => {
   const sql = require('./sql');
   const data = await sql('/promo/' + config.siteid, config, 'promos');
+  console.log(data);
   return data;
 };
